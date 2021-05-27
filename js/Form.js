b43d31cd54b4bb375d4b6d26c8029ea778f77b40
adityakharga <72646575+adityakharga@users.noreply.github.com>
2021-05-27 17:32:24 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,52 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h1');
+    this.title = createElement('h1');
+    this.reset = createButton('Reset');
+   
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  
+  display(){
+ 
+    this.title.html("DUAL FIGHT");
+    this.title.position(displayWidth/2 - 270, 0);
+    this.title.style('color','white')
+
+    this.input.position(displayWidth/2 - 240 , displayHeight/2 - 80);
+    
+    this.button.position(displayWidth/2+66 ,displayHeight/2-80);
+
+    this.reset.position(displayWidth-200,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 230, displayHeight/4);
+      this.greeting.style('font-size', '40px');
+      this.greeting.style('color', 'black');
+    });
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+     
+    });
+
+  }
+}
---
+++ js/Game.js
@@ -0,0 +1,96 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+    w1 = createSprite(100,200);
+    w1.addImage(warriorimg1,"c1.png")
+    w1.scale=0.5
+    w2 = createSprite(500,100);
+    w2.addImage(warriorimg2,"c2.png")
+    w2.scale=0.3
+    wars = [w1, w2];
+  }
+
+  play(){
+    form.hide();
+    
+    Player.getPlayerInfo();
+   
+    
+    if(allPlayers !== undefined){
+    
+      var index = 0;
+      var x = 0 ;
+      var y = 0;
+
+      for(var plr in allPlayers){
+       
+        index = index + 1 ;
+
+        x = x + 200;
+        y = y+200;
+        wars[index-1].x = x;
+        wars[index-1].y = y;
+       
+        if (index === player.index){ 
+          wars[index - 1].shapeColor = "red";      
+          camera.position.x = displayWidth/2;
+          camera.position.y = wars[index-1].y;
+        }
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null)
+    {
+      player.distanceY +=10
+      player.update();
+    }
+    if(keyIsDown(DOWN_ARROW) && player.index !== null)
+    {
+      player.distanceY -=10
+      player.update();
+    }
+    if(keyIsDown(RIGHT_ARROW) && player.index !== null)
+    {
+      player.distanceX +=10
+      player.update();
+    }
+    if(keyIsDown(LEFT_ARROW) && player.index !== null)
+    {
+      player.distanceX -=10
+      player.update();
+    }
+   
+    drawSprites();
+  }
+
+  end(){
+    console.log("Game Ended");
+    console.log(player.rank);
+  }
+}
